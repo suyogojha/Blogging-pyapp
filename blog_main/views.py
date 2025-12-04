1c9c7cf6b41a41c58680452ab1757513beb7af8b
Suyog-Ojha <suyogojha2014@gmail.com>
2025-12-04 22:57:27 +0000

    04/12/025

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,17 @@
 
 from django.shortcuts import render
 
+from blogs.models import *
+
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True)
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+    }
+    return render(request, 'home.html', context)
+
+
+# 1:15:26
