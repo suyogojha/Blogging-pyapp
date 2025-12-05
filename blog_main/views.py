a6aca6cf5d00c247263b48d26eb826fbfdffbe48
Suyog-Ojha <suyogojha2014@gmail.com>
2025-12-05 21:28:14 +0000

    #2 Featured&RecentPOST

--- blog_main/views.py
+++ blog_main/views.py
@@ -6,12 +6,13 @@ from blogs.models import *
 
 def home(request):
     categories = Category.objects.all()
-    featured_posts = Blog.objects.filter(is_featured=True)
+    featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by('created_at')
+    posts = Blog.objects.filter(is_featured=False, status="Published")
     context = {
         'categories': categories,
         'featured_posts': featured_posts,
+        'posts': posts,
     }
     return render(request, 'home.html', context)
 
 
-# 1:15:26
--- blogs/admin.py
+++ blogs/admin.py
@@ -6,7 +6,7 @@ class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'category', 'author', 'status', 'is_featured')
     search_fields = ('id', 'title', 'category__category_name', 'status')
-    list_editable = ('is_featured',)
+    list_editable = ('is_featured','status',)
 
 admin.site.register(Category)
 admin.site.register(Blog,BlogAdmin)
