b097852120a1186fd05961f2986be859b8dcb756
Suyog-Ojha <suyogojha2014@gmail.com>
2025-12-09 22:40:09 +0000

    @Contextprocess

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -5,9 +5,11 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import include    
+from blogs import views as BlogsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
     path('category/', include('blogs.urls')),
+    path('<slug:slug>/', BlogsView.blogs, name='blogs'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -2,14 +2,23 @@
 from django.shortcuts import render
 
 from blogs.models import *
-
+from assignments.models import *
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by('created_at')
     posts = Blog.objects.filter(is_featured=False, status="Published")
+    
+    
+    # fetch about us section data
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+    
     context = {
         'featured_posts': featured_posts,
         'posts': posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
 
